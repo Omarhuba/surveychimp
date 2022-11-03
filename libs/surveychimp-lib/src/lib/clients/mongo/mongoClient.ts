9fee7b553349d0300a189cf3ae880cc6b77ac221
Omarhuba <omaruba@gmail.com>
2022-11-03 10:33:06 +0100

    added new connection string to dev branch

--- libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
+++ libs/surveychimp-lib/src/lib/clients/mongo/mongoClient.ts
@@ -2,9 +2,9 @@
 import mongoose from 'mongoose';
 
 export const connect = async (): Promise<typeof mongoose> => {
-    console.log('MONKEYS_DB', process.env.MONKEYS_DB)
-    if (!process.env.MONKEYS_DB) {
+    console.log('MONKEYS_DB_DEV', process.env.MONKEYS_DB_DEV)
+    if (!process.env.MONKEYS_DB_DEV) {
         throw "500";
     }
-    return mongoose.connect(process.env.MONKEYS_DB);
+    return mongoose.connect(process.env.MONKEYS_DB_DEV);
 }
