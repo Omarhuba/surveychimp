14dec7355567ad6ab08ccc3d8a58168819e37613
ashod.levonian@iths.se <ashod.levonian@iths.se>
2022-11-15 13:48:15 +0100

    added Sentry

--- libs/surveychimp-lib/src/lib/clients/mongo/survey/index.ts
+++ libs/surveychimp-lib/src/lib/clients/mongo/survey/index.ts
@@ -2,5 +2,5 @@ export * from './SurveyModel';
 export * from './addResponseToSurvey';
 export * from './addSurvey';
 export * from './getSurveyById';
-export * from './getSurveys';
+//export * from './getSurveys';
 
