1dac23fff72347c5f6a9ea121352b568084ab88a
ashod.levonian@iths.se <ashod.levonian@iths.se>
2022-11-17 10:41:30 +0100

    added google Analytics event tracker

--- apps/surveychimp-survey-spa/src/app/app.tsx
+++ apps/surveychimp-survey-spa/src/app/app.tsx
@@ -2,12 +2,16 @@
 import styled from 'styled-components';
 import SurveyPage from './pages/SurveyPage';
 import StartPage from './pages/StartPage';
+import {useEffect} from 'react'
 
 
 import { Route, Routes } from 'react-router-dom';
 // import {Rating} from '../components/ReactStar'
 import ReactStars from 'react-stars'
 
+import ReactGA from 'react-ga';
+  const TRACKING_ID = "UA-249863110-1"; // OUR_TRACKING_ID
+  ReactGA.initialize(TRACKING_ID);
 
 const StyledApp = styled.div`
     // Your style here
@@ -15,6 +19,9 @@ const StyledApp = styled.div`
 
 
 export function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <StyledApp>
